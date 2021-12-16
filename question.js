f8afdd07e6d4a995c76eb5e1e988bf9cc96f7271
Ajitjha-823 <95955751+Ajitjha-823@users.noreply.github.com>
2021-12-16 19:07:41 +0530

    Add files via upload
    Quiz app final

---
+++ question.js
@@ -0,0 +1,431 @@
+   
+    let questions = [
+
+    {
+
+    numb: 1,
+
+    question: "1.Who Wrote a Book Called 'Siyasat Me Sadasyata'? 1.हाल ही में किसने 'सियासत में सदस्यता' नामक पुस्तक लिखी है| ",
+
+    answer: "Nitish Kumar नीतीश कुमार",
+
+    options: [
+
+      "Anandiben Patel आनंदीबेन पटेल",
+
+      "Satyapal Malik सत्यपाल मलिक",
+
+      "V. P. Singh Badnore वी. पी. सिंह बदनोर",
+
+      "Nitish Kumar नीतीश कुमार"
+
+    ]
+
+  },
+
+    {
+
+    numb: 2,
+
+    question: "2.Who has been appointed as the President of the Film and Television Institute of India? 2.फिल्म एंड टेलीविजन इंस्टीट्यूट ऑफ इंडिया के अध्यक्ष के रूप में किसे नियुक्त किया गया है?",
+
+    answer: "Shekhar Kapoor शेखर कपूर",
+
+    options: [
+
+      "Anupam Kher अनुपम खेर",
+
+      "Paresh Rawal परेश रावल",
+
+      "Shekhar Kapoor शेखर कपूर",
+
+      "Shakti Kapoor शक्ति  कपूर"
+
+    ]
+
+  },
+
+    {
+
+    numb: 3,
+
+    question: "3.When is World Soil Day? 3.विश्व मृदा दिवस कब मनाया जाता है?",
+
+    answer: "5 December",
+
+    options: [
+
+      "3 December 3 दिसंबर",
+
+      "5 December 5 दिसंबर",
+
+      "10 December 10 दिसंबर",
+
+      "12 December 12 दिसंबर"
+
+    ]
+
+  },
+
+    {
+
+    numb: 4,
+
+    question: "4.Under which scheme did Prime Minister Modi start distributing property cards? 4.प्रधानमंत्री मोदी ने किस योजना के तहत संपत्ति कार्ड वितरित करना शुरू किया?",
+
+    answer: "Swamitva स्वामित्व",
+
+    options: [
+
+      "Sahaj सहज",
+
+      "Saubhagya सौभाग्य",
+
+      "Swamitva स्वामित्व",
+
+      "Sadbhavna सदभावना"
+
+    ]
+
+  },
+
+    {
+
+    numb: 5,
+
+    question: "5.Who was awarded the Nobel Peace Prize 2020? 5.2020 का नोबेल शांति पुरस्कार किसे दिया गया था?",
+
+    answer: "World Food Program (WFP) विश्व खाद्य कार्यक्रम (WFP)",
+
+    options: [
+
+      "World Food Program (WFP) विश्व खाद्य कार्यक्रम (WFP)",
+
+      "Food Corporation of India (FCI) भारतीय खाद्य निगम (FCI)",
+
+      "United Nations संयुक्त राष्ट्र",
+
+      "World Bank विश्व बैंक"
+
+    ]
+
+  },
+
+	{
+
+    numb: 6,
+
+    question: "6.In which place will India's first vertical lift railway bridge be built? 6.भारत का पहला ऊर्ध्वाधर लिफ्ट रेलवे पुल किस स्थान पर बनाया जाएगा?",
+
+    answer: "Rameswaram रामेश्वरम",
+
+    options: [
+
+      "Vizag वैज़ाग",
+
+      "Rameswaram रामेश्वरम",
+
+      "Cuttack कटक",
+
+      "Kochi कोच्चि"
+
+    ]
+
+  },
+
+  {
+
+    numb: 7,
+
+    question: "7.Which of the following commercial banks topped EASE 2.0 Banking Reforms Index (March 2018-2020)? 7.निम्नलिखित में से किस वाणिज्यिक बैंक ने EASE 2.0 बैंकिंग सुधार सूचकांक (मार्च 2018-2020) में सबसे ऊपर है?",
+
+    answer: "Bank of Baroda बैंक ऑफ बड़ौदा",
+
+    options: [
+
+      "Punjab National Bank पंजाब नेशनल बैंक",
+
+      "State Bank of India भारतीय स्टेट बैंक",
+
+      "Bank of baroda बैंक ऑफ बड़ौदा",
+
+      "Canara Bank केनरा बैंक"
+
+    ]
+
+  },
+
+    {
+
+    numb: 8,
+
+    question: "8.Which one of the following countries has hosted the Army Exercise TSENTR, 2019? 8.निम्नलिखित में से किस देश ने आर्मी एक्सरसाइज TSENTR, 2019 की मेजबानी की है?",
+
+    answer: "Russia रूस",
+
+    options: [
+
+      "Russia रूस",
+
+      "China चीन",
+
+      "Kazakhstan कजाखस्तान",
+
+      "Kyrgyzstan किर्गिज़स्तान"
+
+    ]
+
+  },
+
+    {
+
+    numb: 9,
+
+    question: "9.Naseem-Al-Bahr is a bilateral naval exercise between India and  9.नसीम-अल-बह्र भारत और के बीच एक द्विपक्षीय नौसैनिक अभ्यास है",
+
+    answer: "Oman ओमान",
+
+    options: [
+
+      "United Arab Emirates संयुक्त अरब अमीरात",
+
+      "Iran ईरान",
+
+      "Saudi Arabia सऊदी अरब",
+
+      "Oman ओमान"
+
+    ]
+
+  },
+
+     {
+
+    numb: 10,
+
+    question: "10.Till when does the government want to eliminate the Tuberculosis? 10.कब तक सरकार तपेदिक को खत्म करना चाहती है?",
+
+    answer: "2025 २०२५",
+
+    options: [
+
+      "2030 २०३०",
+
+      "2025 २०२५",
+
+      "2027 २०२७",
+
+      "2022 २०२२"
+
+    ]
+
+  },
+
+       {
+
+    numb: 11,
+
+    question: "11.Which city emerged on top of the IMD Smart City Index, 2020? 11.आईएमडी स्मार्ट सिटी इंडेक्स, 2020 के शीर्ष पर कौन सा शहर उभरा?",
+
+    answer: "Singapore सिंगापुर",
+
+    options: [
+
+      "Dubai दुबई",
+
+      "Singapore सिंगापुर",
+
+      "Hongkong हॉगकॉग",
+
+      "Mumbai मुंबई"
+
+    ]
+
+  },
+
+       {
+
+    numb: 12,
+
+    question: "12.Which country emerged on top of the Global Innovation Index, 2020? 12.ग्लोबल इनोवेशन इंडेक्स, 2020 के शीर्ष पर कौन सा देश उभरा?",
+
+    answer: "Switzerland स्विट्ज़रलैंड",
+
+    options: [
+
+      "Finland फिनलैंड",
+
+      "Sweden स्वीडन",
+
+      "Switzerland स्विट्ज़रलैंड",
+
+      "USA अमेरीका"
+
+    ]
+
+  },
+
+       {
+
+    numb: 13,
+
+    question: "13.India's longest river ropeway for passengers was inaugurated in 13.यात्रियों के लिए भारत की सबसे लंबी नदी रोपवे का उद्घाटन किया गया",
+
+    answer: "Guwahati गुवाहाटी",
+
+    options: [
+
+      "Shimla शिमला",
+
+      "Manali मनाली",
+
+      "Rishikesh ऋषिकेश",
+
+      "Guwahati गुवाहाटी"
+
+    ]
+
+  },
+
+       {
+
+    numb: 14,
+
+    question: "14.The first floating solar power plant in Uttar Pradesh is built on which of the following dams? 14.उत्तर प्रदेश में पहला तैरता हुआ सौर ऊर्जा संयंत्र निम्नलिखित में से किस बाँध पर बनाया गया है?",
+
+    answer: "Rihand Dam रिहंद बांध",
+
+    options: [
+
+      "Rihand Dam रिहंद बांध",
+
+      "Matatila Dam माताटीला डैम",
+
+      "Rajghat Dam राजघाट बांध",
+
+      "Dhanraul Dam धनरोल बांध"
+
+    ]
+
+  },
+
+       {
+
+    numb: 15,
+
+    question: "15.Which part of the Constitution of India declares the ideal of Welfare State? 15.भारत के संविधान का कौन सा भाग वेलफेयर स्टेट के आदर्श की घोषणा करता है?",
+
+    answer: "Directive Principles of State Policy राज्य नीति के निर्देशक सिद्धांत",
+
+    options: [
+
+      "Fundamental Rights मौलिक अधिकार",
+
+      "Directive Principles of State Policy राज्य नीति के निर्देशक सिद्धांत",
+
+      "Preamble प्रस्तावना",
+
+      "Seventh Schedule सातवीं अनुसूची"
+
+    ]
+
+  },
+
+        {
+
+    numb: 16,
+
+    question: "16.Which of the following is India's first Kisan Rail? 16.निम्नलिखित में से कौन भारत की पहली किसान रेल है?",
+
+    answer: "Devlali-Danapur Kisan Rail देवलाली-दानापुर किसान रेल",
+
+    options: [
+
+      "Anantpur-Adarsh Nagar Kisan Rail अनंतपुर-आदर्श नगर किसान रेल",
+
+      "Yashwanatpur-Ajmer Kisan Rail यशवंतपुर-अजमेर किसान रेल",
+
+      "Devlali-Danapur Kisan Rail देवलाली-दानापुर किसान रेल",
+
+      "Bhatainda-Bhopal Kisan Rail भटैनादा-भोपाल किसान रेल"
+
+    ]
+
+  },
+
+        {
+
+    numb: 17,
+
+    question: "17.Which nation declared a state of environmental emergency on August 7, 2020? 17.7 अगस्त, 2020 को किस राष्ट्र ने पर्यावरणीय आपातकाल की स्थिति घोषित की?",
+
+    answer: "Maldives मालदीव",
+
+    options: [
+
+      "Malaysia मलेशिया",
+
+      "Indonesia इंडोनेशिया",
+
+      "Mauritius मॉरीशस",
+
+      "Maldives मालदीव"
+
+    ]
+
+  },
+
+        {
+
+    numb: 18,
+
+    question: "18.Which State/ UT will host the International Film Festival of India 2020? 18.कौन सा राज्य / केंद्र शासित प्रदेश भारत के अंतर्राष्ट्रीय फिल्म महोत्सव 2020 की मेजबानी करेगा?",
+
+    answer: "Goa गोवा",
+
+    options: [
+
+      "Ladakh लद्दाख",
+
+      "Karnataka कर्नाटक",
+
+      "Goa गोवा",
+
+      "Assam असम"
+
+    ]
+
+  },
+
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+
+
+  //   {
+
+  //   numb: 6,
+
+  //   question: "Your Question is Here",
+
+  //   answer: "Correct answer of the question is here",
+
+  //   options: [
+
+  //     "Option 1",
+
+  //     "option 2",
+
+  //     "option 3",
+
+  //     "option 4"
+
+  //   ]
+
+  // },
+
+];
+
+
+    
